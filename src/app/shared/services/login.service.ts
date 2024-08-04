a2776490c8395ea19be3ddfd5de027784a329247
David Alves Dutra <dadutra@hotmail.com>
2024-08-04 18:32:45 -0300

    Hotfix. Msg alert.

---
+++ src/app/shared/services/login.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor() { }
+
+  usuarioLogado!: {email: string, senha: string}
+
+  login(usuario: {email: string, senha: string}): boolean {
+    for(let usr of this.usuarios) {
+      if((usr.email === usuario.email) && (usr.senha === usuario.senha)) {
+        sessionStorage.setItem('usuarioLogado', JSON.stringify(usuario));
+        sessionStorage.setItem('emailLogado', usuario.email);
+        alert('Usuário logado com sucesso!')
+        return true;
+      }
+    }
+    return false;
+}
+
+getLogado() {
+  return this.usuarioLogado;
+}
+
+logout() {
+  sessionStorage.removeItem('usuarioLogado')
+}
+
+
+  usuarios = [
+    {
+      email: 'jose@gmail.com',
+      senha: 'jose123',
+      perfil: 'Aluno'
+    },
+    {
+      email: 'adm@adm.com',
+      senha: 'adm',
+      perfil: 'Administrador'
+    },
+    {
+      email: 'paulo@adm.com',
+      senha: 'paulo123',
+      perfil: 'Docente'
+    }
+  ]
+}
